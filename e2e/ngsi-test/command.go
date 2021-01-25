6222401db75c949cf78d0812f55054511784060b
Kazuhito Suda <kazuhito@fisuda.jp>
2021-01-25 09:42:11 +0900

    Update ngsi-test command

--- e2e/ngsi-test/command.go
+++ e2e/ngsi-test/command.go
@@ -31,6 +31,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -49,13 +50,14 @@ var cmdTable map[string]cmdDef
 
 func initCmdTable() {
 	cmdTable = map[string]cmdDef{
-		"```":   compareCmd,
-		"halt":  haltCmd,
-		"http":  httpCmd,
-		"ngsi":  ngsiCmd,
-		"print": printCmd,
-		"sleep": sleepCmd,
-		"wait":  waitCmd,
+		"```":        compareCmd,
+		"createdata": createdataCmd,
+		"halt":       haltCmd,
+		"http":       httpCmd,
+		"ngsi":       ngsiCmd,
+		"print":      printCmd,
+		"sleep":      sleepCmd,
+		"wait":       waitCmd,
 	}
 }
 
@@ -66,7 +68,7 @@ func compareCmd(line int, args []string) error {
 
 	line = line - len(args) + 1
 
-	if len(args[0]) < 3 {
+	if len(args[0]) < 4 {
 		return &ngsiCmdError{funcName, 1, "expected code error", nil}
 	}
 	expectedCode := args[0][3:]
@@ -124,7 +126,7 @@ func httpCmd(line int, args []string) error {
 	default:
 		return &ngsiCmdError{funcName, 3, "http verb error", nil}
 	case "get":
-		return httpRequest(http.MethodGet, nil, args)
+		return httpRequest(http.MethodGet, url, nil, nil)
 	case "post":
 		if len(args) < 4 {
 			return &ngsiCmdError{funcName, 4, "http post url --data \"{\"data\":\"post data\"}", nil}
@@ -133,9 +135,9 @@ func httpCmd(line int, args []string) error {
 			return &ngsiCmdError{funcName, 5, "http post url --data \"{\"data\":\"post data\"}", nil}
 		}
 		header := map[string]string{"Content-Type": "application/json"}
-		return httpRequest(http.MethodPost, header, args)
+		return httpRequest(http.MethodPost, url, header, []byte(args[4]))
 	case "delete":
-		return httpRequest(http.MethodDelete, nil, args)
+		return httpRequest(http.MethodDelete, url, nil, nil)
 	}
 
 }
@@ -219,10 +221,23 @@ func waitCmd(line int, args []string) (err error) {
 	const funcName = "waitCmd"
 
 	retry := 600
-	if len(args) == 2 {
+	status := ""
+	if len(args) > 1 {
 		if !isHTTP(args[1]) {
 			return &ngsiCmdError{funcName, 1, "url error: " + args[1], nil}
 		}
+		opts, err := getOpts(args[2:], []string{"retry", "status"})
+		if err != nil {
+			return &ngsiCmdError{funcName, 2, err.Error(), nil}
+		}
+		if v, ok := opts["retry"]; ok {
+			retry, err = strconv.Atoi(v)
+			if err != nil || retry < 0 {
+				return &ngsiCmdError{funcName, 3, err.Error(), nil}
+			}
+		}
+		status = opts["status"]
+
 		fmt.Printf("Waiting for response from %s\n", args[1])
 		for {
 			var res *http.Response
@@ -230,34 +245,45 @@ func waitCmd(line int, args []string) (err error) {
 			if err != nil {
 				retry--
 				if retry == 0 {
-					return &ngsiCmdError{funcName, 2, "no response from " + args[1], nil}
+					return &ngsiCmdError{funcName, 4, "no response from " + args[1], nil}
 				}
 				time.Sleep(time.Second * time.Duration(1))
 				continue
 			}
-			defer func() { setNewError(funcName, 3, res.Body.Close(), &err) }()
+			defer func() { setNewError(funcName, 5, res.Body.Close(), &err) }()
 
-			_, err = ioutil.ReadAll(res.Body)
+			b, err := ioutil.ReadAll(res.Body)
 			if err != nil {
-				return &ngsiCmdError{funcName, 4, err.Error(), err}
+				return &ngsiCmdError{funcName, 6, err.Error(), err}
+			}
+			if status != "" {
+				j := map[string]interface{}{}
+				err = json.Unmarshal(b, &j)
+				if err != nil {
+					return &ngsiCmdError{funcName, 7, err.Error(), nil}
+				}
+				if v, ok := j["status"]; ok {
+					if status == v.(string) {
+						return nil
+					}
+				} else {
+					return &ngsiCmdError{funcName, 8, "status not found", nil}
+				}
+
 			}
 			if res.StatusCode == http.StatusOK || res.StatusCode == http.StatusNoContent {
 				return nil
 			}
 		}
 	}
-	return &ngsiCmdError{funcName, 4, "param error" + args[1], nil}
+	return &ngsiCmdError{funcName, 9, "param error" + args[1], nil}
 }
 
-func httpRequest(method string, header map[string]string, args []string) (err error) {
+func httpRequest(method string, url string, header map[string]string, b []byte) (err error) {
 	const funcName = "httpRequest"
 
-	b := []byte(nil)
-	if method == http.MethodPost {
-		b = []byte(args[4])
-	}
 	var req *http.Request
-	req, err = http.NewRequest(method, args[2], bytes.NewBuffer(b))
+	req, err = http.NewRequest(method, url, bytes.NewBuffer(b))
 	if err != nil {
 		return &ngsiCmdError{funcName, 1, err.Error(), err}
 	}
@@ -277,10 +303,12 @@ func httpRequest(method string, header map[string]string, args []string) (err er
 	if err != nil {
 		return &ngsiCmdError{funcName, 4, err.Error(), err}
 	}
-
 	status := "0"
 	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusNoContent && res.StatusCode != http.StatusCreated {
 		status = "1"
+		if len(b) > 0 {
+			fmt.Println(string(b))
+		}
 	}
 	val["?"] = []string{status}
 
@@ -291,9 +319,73 @@ func httpRequest(method string, header map[string]string, args []string) (err er
 		val["$"] = []string{}
 	}
 
-	return nil
+	if status == "0" {
+		return nil
+	}
+	return &ngsiCmdError{funcName, 5, "httpRequest Error", err}
 }
 
 func isHTTP(url string) bool {
 	return strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")
+}
+
+func getOpts(args []string, optsList []string) (map[string]string, error) {
+	const funcName = "getOpts"
+
+	opts := map[string]string{}
+
+	i := 0
+	for {
+		v, err := getArg(args, &i)
+		if err != nil {
+			break
+		}
+		if !strings.HasPrefix(v, "-") {
+			return nil, &ngsiCmdError{funcName, 2, "option error: " + v, nil}
+		}
+		v = v[1:]
+		v = strings.TrimPrefix(v, "-")
+		p, err := getArg(args, &i)
+		if err != nil {
+			return nil, &ngsiCmdError{funcName, 3, "option error: " + v, nil}
+		}
+		if contains(optsList, v) {
+			opts[strings.ToLower(v)] = p
+		} else {
+			return nil, &ngsiCmdError{funcName, 4, "unknown option: " + v, nil}
+		}
+	}
+	return opts, nil
+}
+
+func getArg(args []string, i *int) (string, error) {
+	const funcName = "getArg"
+
+	if *i >= len(args) {
+		return "", &ngsiCmdError{funcName, 1, "arg error", nil}
+	}
+	*i++
+	return args[*i-1], nil
+}
+
+func contains(a []string, e string) bool {
+	e = strings.ToLower(e)
+	for _, v := range a {
+		if e == strings.ToLower(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func checkRequiredOpt(opts map[string]string, list []string) error {
+	const funcName = "checkRequiedOpt"
+
+	for _, k := range list {
+		_, ok := opts[k]
+		if !ok {
+			return &ngsiCmdError{funcName, 1, k + " not found", nil}
+		}
+	}
+	return nil
 }
